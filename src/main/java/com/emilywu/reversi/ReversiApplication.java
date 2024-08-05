5546754044ee639ac9e668d9a0fa74298d3e0da5
Emily Wu <emilywu@Emilys-MacBook-Pro.local>
2024-08-05 13:45:20 -0400

    import springboot project

---
+++ src/main/java/com/emilywu/reversi/ReversiApplication.java
@@ -0,0 +1,13 @@
+package com.emilywu.reversi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReversiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReversiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/emilywu/reversi/ReversiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emilywu.reversi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReversiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
