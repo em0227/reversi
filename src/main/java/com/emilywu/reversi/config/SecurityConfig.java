4f845827c67ebd12ac868359bb37659716249669
em0227 <emilywu0227@gmail.com>
2024-12-05 21:23:16 -0500

    oauth first draft

---
+++ src/main/java/com/emilywu/reversi/config/SecurityConfig.java
@@ -0,0 +1,38 @@
+package com.emilywu.reversi.config;
+
+import com.emilywu.reversi.CustomLogoutHandler;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.client.oidc.web.logout.OidcClientInitiatedLogoutSuccessHandler;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests(auth -> {
+                    auth.requestMatchers("/").permitAll();
+                    auth.requestMatchers("/favicon.ico").permitAll();
+                    auth.anyRequest().authenticated();
+                })
+                .oauth2Login(withDefaults())
+                .logout(logout -> logout
+                        .logoutUrl("/logout") // Custom logout URL
+                        .addLogoutHandler(new CustomLogoutHandler()) // Add your custom handler
+                        .logoutSuccessUrl("/login?logout")
+                )
+                .build();
+    }
+
+}
--- src/main/java/com/emilywu/reversi/game/GameController.java
+++ src/main/java/com/emilywu/reversi/game/GameController.java
@@ -3,11 +3,16 @@ package com.emilywu.reversi.game;
 import com.emilywu.reversi.game.dto.CreateGameRequestDto;
 import com.emilywu.reversi.game.dto.GameBoardDto;
 import com.emilywu.reversi.game.dto.UpdateGameRequestDto;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
+import java.util.Map;
 import java.util.UUID;
 
 @RestController
@@ -15,6 +20,13 @@ import java.util.UUID;
 public class GameController {
     @Autowired
     private GameService gameService;
+
+    @GetMapping("/")
+    public Map<String, Object> currentPlayer(OAuth2AuthenticationToken token) {
+        return token.getPrincipal().getAttributes();
+        //String = token.getPrincipal().getAttributes().get("email");
+    }
+
 
     @GetMapping("/{game_id}")
     public ResponseEntity<GameBoardDto> getGame (@PathVariable UUID game_id) throws IOException {
